125f3762bc52769da802b385c22635f2b45c4118
Nawabsaad96 <nawabsaad96@gmail.com>
2026-07-31 08:37:53 +0530

    output

---
+++ LAB/Unit 1/exp 03.py
@@ -0,0 +1,13 @@
+n = 5
+
+fact = 1
+for i in range(1, n + 1):
+    fact *= i
+print("Iterative:", fact)
+
+def factorial(n):
+    if n == 0:
+        return 1
+    return n * factorial(n - 1)
+
+print("Recursive:", factorial(n))
